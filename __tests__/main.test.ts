fa1953176df55caa613f73b33bb38d61cc4240d0
Nick Ethier <nethier@hashicorp.com>
2021-10-28 23:34:46 -0400

    initial hc tools action

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,29 +1,92 @@
-import {wait} from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
-import {expect, test} from '@jest/globals'
+import io = require('@actions/io')
+import fs = require('fs')
+import os = require('os')
+import path = require('path')
+import * as installer from '../src/installer'
+import nock from 'nock'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
+const toolDir = path.join(
+  __dirname,
+  'runner',
+  path.join(Math.random().toString(36).substring(7)),
+  'tools'
+)
+const tempDir = path.join(
+  __dirname,
+  'runner',
+  path.join(Math.random().toString(36).substring(7)),
+  'temp'
+)
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
+process.env['RUNNER_TOOL_CACHE'] = toolDir
+process.env['RUNNER_TEMP'] = tempDir
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+const IS_WINDOWS = process.platform === 'win32'
+const PRODUCT_NAME = 'otto'
+const metadataIndex = require('./metadata.json')
+
+describe('install tests', () => {
+  beforeAll(function () {
+    // Mock out the metadata request for all tests
+    nock(installer.releasesUrl())
+      .persist()
+      .get(`/index.json`)
+      .reply(200, metadataIndex)
+    // We don't want any real http requests in the tests
+    nock.disableNetConnect()
+  })
+
+  beforeEach(async function () {
+    await io.rmRF(toolDir)
+    await io.rmRF(tempDir)
+    await io.mkdirP(toolDir)
+    await io.mkdirP(tempDir)
+  })
+
+  afterEach(function () {
+    jest.resetAllMocks()
+    jest.clearAllMocks()
+  })
+
+  afterAll(async function () {
+    await io.rmRF(toolDir)
+    await io.rmRF(tempDir)
+  })
+
+  it('attempts to download the tool if no version is found in the cache', async () => {
+    const download = nock(installer.releasesUrl())
+      .persist()
+      .get(
+        `/${PRODUCT_NAME}/0.1.0/${PRODUCT_NAME}_0.1.0_${os.platform()}_amd64.zip`
+      )
+      .replyWithFile(200, `${__dirname}/product.zip`, {
+        'Content-Type': 'application/zip'
+      })
+
+    await installer.getBinary(PRODUCT_NAME, '0.1.0')
+
+    expect(download.isDone()).toBe(true)
+  })
+
+  it('errors with a version that does not exist', async () => {
+    let thrown = false
+    try {
+      await installer.getBinary(PRODUCT_NAME, '0.100.0')
+    } catch {
+      thrown = true
+    }
+    expect(thrown).toBe(true)
+  })
+
+  it('uses versions in the cache', async () => {
+    const toolPath: string = path.join(
+      toolDir,
+      PRODUCT_NAME,
+      '0.1.0',
+      os.arch()
+    )
+    await io.mkdirP(toolPath)
+    fs.writeFileSync(`${toolPath}.complete`, 'hello')
+    await installer.getBinary(PRODUCT_NAME, '0.1.0')
+  })
 })
